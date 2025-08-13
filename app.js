ed5b7c204bf01c6bb6ae178f2805fe13a4a9b777
Aun-Ali-12 <aunali29331@gmail.com>
2025-08-14 00:55:47 +0500

    auth added

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ if (suForm) {
 
 // onclick login btn on index html
 if (logBtn) {
-  logBtnBtn.addEventListener("click", () => {
+  logBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     window.location.href = "./form.html";
-    liForm.style.display = "block";
   });
 }
 
-let suBtn = document.getElementById("signup");
 //onclick signup btn in loginform
+let suBtn = document.getElementById("signup");
 if (suBtn) {
   suBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -27,8 +27,8 @@ if (suBtn) {
   });
 }
 
+//onclick signin btn in Signupform
 let siBtn = document.getElementById("signin");
-//onclick signup btn in loginform
 if (siBtn) {
   siBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -46,25 +46,72 @@ let cPass = document.getElementById("c-pass");
 let cName = document.getElementById("c-name");
 
 if (cAcc) {
+  //on click signup button
   cAcc.addEventListener("click", (e) => {
     e.preventDefault();
     createAcc();
   });
-  cAcc.addEventListener("keydown", (event) => {
-    e.preventDefault();
-    if (event.key == "Enter") {
-      createAcc();
-    }
-  });
 }
 
+//Function to create an account:
 async function createAcc() {
   const { data, error } = await client.auth.signUp({
     email: cEmail.value,
     password: cPass.value,
     options: {
-      name: cName.value,
+      data: {
+        name: cName.value,
+      },
     },
   });
-  error? console.error(error.message):  console.log(cEmail.value, cPass.value, cName.value);
+  error ? alert(error.message) : alert("Account created successfully"),
+    (suForm.style.display = "none");
+  liForm.style.display = "block";
 }
+
+/******************* Login Functionality: ********************/
+let liMail = document.getElementById("l-email");
+let liPass = document.getElementById("l-pass");
+if (document.getElementById("loginAcc")) {
+  document.getElementById("loginAcc").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginAcc();
+  });
+}
+//Function to perform login functionality:
+let userName = document.getElementById("show");
+async function loginAcc() {
+  const { data, error } = await client.auth.signInWithPassword({
+    email: liMail.value,
+    password: liPass.value,
+  });
+  error ? alert(error.message) : alert("Login successfull"),
+    (window.location.href = "profile.html");
+}
+
+/***************** Sign out functionality ********************/
+if (document.getElementById("logout")) {
+  document.getElementById("logout").addEventListener("click", async () => {
+    const { data, error } = await client.auth.getSession();
+    !error ? (window.location.href = "form.html") : alert(error.message);
+  });
+}
+
+//Auth check:
+async function authCheck() {
+  const {
+    data: { session },
+    error,
+  } = await client.auth.getSession();
+  !session
+    ? alert("First Login")((window.location.href = "form.html"))
+    : console.log(session);
+  //user name on profile:
+  userName.innerHTML = `<p>${session.user.user_metadata.name}</p><p>${session.user.user_metadata.email}</p>`;
+}
+//checking auth func on specific pathname
+if (window.location.pathname == "/profile.html") {
+  authCheck();
+}
+
+/************* User content posting  ********/
