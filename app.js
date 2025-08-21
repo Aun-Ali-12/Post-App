903864d2f2447e9558e4286c09cecb6c26f9cc70
Aun-Ali-12 <aunali29331@gmail.com>
2025-08-22 01:54:11 +0500

    onclick user name, user profile will be shown

--- app.js
+++ app.js
@@ -126,10 +126,33 @@ if (window.location.pathname == "/profile.html") {
 }
 
 /************* User content posting  ********/
-let image = document.getElementById("image");
-let caption = document.getElementById("caption");
-let postBtn = document.getElementById("post");
-let showContent = document.getElementById("content-container");
+let image = document.getElementById("image"); //input field for uploading pic for post
+let caption = document.getElementById("caption"); //input field for text
+let postBtn = document.getElementById("post"); //btn through which post will be posted
+let showContent = document.getElementById("content-container"); //div in which only logged in user's post will be shown
+let createPostBtn = document.getElementById("createPost"); //Btn to show post container
+let postContainer = document.getElementById("post-container"); //container which includes posting inputs
+let closeBtn = document.getElementById("close"); //close btn in post container
+
+//initially post container will be hidden
+if (postContainer) {
+  postContainer.classList.add("hidden");
+}
+
+//Onclick create account button, post container will be shown:
+if (createPostBtn) {
+  createPostBtn.addEventListener("click", () => {
+    postContainer.classList.add("show");
+  });
+}
+
+//onclick close btn in post container
+if (postContainer) {
+  closeBtn.addEventListener("click", () => {
+    postContainer.classList.remove("show");
+  });
+}
+
 if (postBtn) {
   postBtn.addEventListener("click", async (e) => {
     e.preventDefault();
@@ -183,6 +206,7 @@ if (postBtn) {
   });
 }
 
+let postCount = document.getElementById("post-count");
 //Func executes on user profile(retreives only loggedin user posts )
 async function userSpecificPost() {
   //get user:
@@ -197,10 +221,14 @@ async function userSpecificPost() {
     .select("*")
     .eq("user_id", userId);
   userpost.forEach((value) => {
+    postCount.innerHTML = `Post ${userpost.length}`;
     showContent.innerHTML += `
-  <p>${value.caption}</p>
+    <div>
+    <p>${user.user_metadata.full_name}</p>
   <img src = "${value.image_url}" width="100px" height="100px">
-  `;
+    <p>${value.caption}</p>
+  </div>
+    `;
   });
 }
 if (showContent) {
@@ -209,46 +237,193 @@ if (showContent) {
   });
 }
 
-//Show all posts:
-let allPost = document.getElementById("show-all-post");
-async function fetch() {
-  const { data: allpost, error } = await client.from("Post").select("*");
-  if (error) {
-    alert("Something is wrong");
-    return;
-  }
-  allpost.innerHTML = "";
-  allpost.forEach((value) => {
-    allPost.innerHTML += `
-    <p>${value.caption}</p>
-  <img src = "${value.image_url}" width="100px" height="100px">
-  `;
+//navigation to home page:
+let homePBtn = document.getElementById("home");
+if (homePBtn) {
+  homePBtn.addEventListener("click", () => {
+    window.location.href = "/home.html";
   });
 }
+
+//Show all posts:
+let allPost = document.getElementById("show-all-post");
 if (allPost) {
   window.addEventListener("DOMContentLoaded", () => {
     fetch();
   });
 }
 
-//navigation to home page:
-let homePBtn = document.getElementById("home");
-homePBtn.addEventListener("click", () => {
-  window.location.href = "/home.html";
-});
-
 //user specific profile name with post:
-// async function postName(){
-//   const { data: postName, error } = await client
-//   .from('Post')
-//   .select(`user_id,
-//     caption,
-//     image_url,
-//     created_at,
-//     profile(
-//       full_name
-//     )`
-//   )
-//     postName? console.log(postName): alert(error.message)
+async function fetch() {
+  const { data: postName, error } = await client.from("Post").select(`user_id,
+    caption,
+    image_url,
+    created_at,
+    profile(
+      full_name, 
+      profile_img
+    )`);
+  let userId = postName.user_id;
+  allPost.innerHTML = "";
+  postName
+    ? postName.forEach((value) => {
+        console.log(value);
+        allPost.innerHTML += `
+        <img src = "${value.profile.profile_img}" width = "50px" height = "50px"/><p class="username" data-userid = "${value.user_id}">${value.profile.full_name}</p>
+       <img src = "${value.image_url}" width="100px" height="100px">
+       <p>${value.caption}</p>
+  
+  `;
+      })
+    : alert(error.message);
+}
+
+//user profile:
+let profileInput = document.getElementById("profile-img");
+let profilePic = document.getElementById("profile-pic");
+let shownPic = document.getElementById("profile");
+if (profilePic) {
+  profilePic.addEventListener("click", () => {
+    uploadProfile();
+  });
+  window.addEventListener("DOMContentLoaded", () => {
+    renderProfile();
+  });
+}
+
+async function uploadProfile() {
+  //get user:
+  const {
+    data: { user },
+  } = await client.auth.getUser();
+  let userId = user.id;
+  let profileObj = profileInput.files[0];
+  let folderName = `profile/${userId}-${Date.now()}`;
+  console.log(userId);
+
+  //insert profile into buccket
+  const { data: insertprofile, error } = await client.storage
+    .from("uploadfile")
+    .upload(`${folderName}`, profileObj);
+  insertprofile ? alert("Ye!!") : alert(error.message);
+  console.log(insertprofile);
+
+  //retreive url of profile picture:
+  const { data: pic_url } = client.storage
+    .from("uploadfile")
+    .getPublicUrl(`${folderName}`);
+  pic_url ? console.log(pic_url) : console.error(error.message);
+
+  let profileUrl = pic_url.publicUrl;
+  // console.log(profileUrl);
+
+  //saving this profile url into profile table:
+  const { error: insertError } = await client
+    .from("profile")
+    .update({
+      user_id: userId,
+      profile_img: profileUrl,
+    })
+    .eq("user_id", userId) //because supa base requires where clause
+    .select();
+  !insertError
+    ? console.log("profile added")
+    : console.log(insertError.message);
+}
+
+let deafultProfile = "./asset/default-profile.png";
+async function renderProfile() {
+  const {
+    data: { user },
+  } = await client.auth.getUser();
+  let userId = user.id;
+
+  const { data: profileData, error: profileError } = await client
+    .from("profile")
+    .select("profile_img, full_name")
+    .eq("user_id", userId)
+    .single();
+  if (profileError) {
+    alert(profileError.message);
+  } else if (profileData && profileData.profile_img) {
+    shownPic.src = profileData.profile_img;
+  } else {
+    shownPic.src = deafultProfile;
+  }
+}
+
+//function used to create a user specific page:
+let getPost = document.getElementById("show-posts");
+let getProfile = document.getElementById("show-profile");
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("username")) {
+    let userId = e.target.dataset.userid; //Gets user id on click
+    window.location.href = `userProfile.html?userId=${userId}`;
+  }
+});
+//param converts query in an easy object
+let param = new URLSearchParams(window.location.search);
+let userId = param.get("userId"); //get userId
+if (userId) {
+  loadUserProfile(userId);
+}
+async function loadUserProfile(userId) {
+  //Fetch user profile:
+  const { data: profile, error: profileError } = await client
+    .from("profile")
+    .select("full_name, profile_img")
+    .eq("user_id", userId)
+    .single();
+
+  profileError
+    ? console.log(profileError.message)
+    : console.log(profile, "Profile fetched");
+
+  //Fetch Posts:
+  const { data: posts, error: postsError } = await client
+    .from("Post")
+    .select("caption, image_url, created_at")
+    .eq("user_id", userId)
+    .order("created_at", { ascending: false });
+
+  if (postsError) {
+    console.log(postsError.message);
+    return;
+  }
+  // console.log(posts);
+
+  //ui render:
+  // window.location.href = "userProfile.html";
+  getProfile.innerHTML = `
+  <div class="profile-header">
+    <img src="${profile.profile_img}" width="80px" height="80px"/>
+    <h2>${profile.full_name}</h2>
+  </div>
+  `;
+
+  posts.forEach((value) => {
+    getPost.innerHTML += `
+    <div class="post">
+      <img src="${value.image_url}" width="150px" height="150px"/>
+      <p>${value.caption}</p>
+    </div>
+  `;
+  });
+}
+
+// //loader
+// let loader = document.getElementById("loader");
+// loader.style.display = "none"
+// if (loader) {
+//   loader.classList.add("hidden");
 // }
-// postName()
+
+// // //show loader func
+// // function showLoader() {
+// //   loader.classList.remove("hidden");
+// // }
+
+// // //hide loader function:
+// // function hideLoader() {
+// //   loader.classList.add("hidden");
+// // }
